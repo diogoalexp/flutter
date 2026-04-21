d0c78237ef672e51c21f0bee63412be9be880eab
Diogo Pereira <diogo.pereira@ezoqc.com>
2026-04-21 13:46:53 -0400

    defining variables

--- roll_dice/lib/gradient_container.dart
+++ roll_dice/lib/gradient_container.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:roll_dice/styled_text.dart';
 
+Alignment startAligment = Alignment.topLeft; //typed
+var endAligment = Alignment.bottomRight; //typed in assignation
+final firstColor = Color.fromARGB(
+  255,
+  84,
+  47,
+  168,
+); //typed in runtime, can accept functions result but will not be reassigned
+const secondColor = Color.fromARGB(
+  255,
+  45,
+  7,
+  98,
+); //typed in compilation time, cannot be reassigned
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 84, 47, 168),
-            Color.fromARGB(255, 45, 7, 98),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          colors: [firstColor, secondColor],
+          begin: startAligment,
+          end: endAligment,
         ),
       ),
       child: Center(child: StyledText()),
