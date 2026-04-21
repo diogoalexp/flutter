4f9700a858fbf056df79a25da83e90f8c04a0820
Diogo Pereira <diogo.pereira@ezoqc.com>
2026-04-21 16:08:50 -0400

    adding icon to a button

--- quiz/lib/start_screen.dart
+++ quiz/lib/start_screen.dart
@@ -21,10 +21,11 @@ class StartScreen extends StatelessWidget {
             ),
           ),
           SizedBox(height: 20),
-          OutlinedButton(
+          OutlinedButton.icon(
             onPressed: onClick,
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
-            child: Text('Start Quiz'),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
           ),
         ],
       ),
