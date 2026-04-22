20c8c3a0e723fc4ab20d1b8e741c6caa3e1d0f06
Diogo Pereira <diogo.pereira@ezoqc.com>
2026-04-22 08:46:55 -0400

    initState

--- quiz/lib/start_screen.dart
+++ quiz/lib/start_screen.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(BuildContext context) {
-    void onClick() {}
-
     return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
@@ -36,7 +36,7 @@ class StartScreen extends StatelessWidget {
           ),
           SizedBox(height: 20),
           OutlinedButton.icon(
-            onPressed: onClick,
+            onPressed: startQuiz,
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
             icon: const Icon(Icons.arrow_right_alt),
             label: const Text('Start Quiz'),
