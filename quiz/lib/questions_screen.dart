1514cffc76b915cb540aea8555e650d268b77bc9
Diogo Pereira <diogo.pereira@ezoqc.com>
2026-04-22 10:02:30 -0400

    dynamic list of widgets

--- quiz/lib/questions_screen.dart
+++ quiz/lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz/answer_button.dart';
+import 'package:quiz/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -13,16 +14,18 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreen extends State<QuestionsScreen> {
   @override
   Widget build(context) {
+    final currentQuestion = questions[0];
+
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('The question..', style: TextStyle(color: Colors.white)),
+          Text(currentQuestion.text, style: TextStyle(color: Colors.white)),
           const SizedBox(height: 30),
-          AnswerButton(answertext: 'test 1', onTap: () {}),
-          AnswerButton(answertext: 'test 1', onTap: () {}),
-          AnswerButton(answertext: 'test 1', onTap: () {}),
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(answertext: answer, onTap: () {});
+          }),
         ],
       ),
     );
